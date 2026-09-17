5add3d31498f3fcfc1bef62c78c090cb65d63a07
RyoF20 <ryofebriano12345@gmail.com>
2026-09-17 19:37:12 +0700

    Versi 1

---
+++ asyncawait.dart
@@ -0,0 +1,62 @@
+
+void main () {
+// ! Variabel wajib ada nilai ketika ada proses
+// stdout.writeln('Masukkan Angka 1 :');
+// var input1 = stdin.readLineSync()!;
+
+// stdout.writeln('Masukkan Angka 2 :');
+// var input2 = stdin.readLineSync()!;
+
+// num hasil = int.parse(input1) + int.parse(input2);
+
+// print(hasil);
+
+//?
+
+// String? namadepan;
+// String? namabelakang;
+// namadepan = 'Wendy';
+// namabelakang = 'Wen';
+// String namalengkap = namadepan + namabelakang;
+
+// String? nama;
+// print(nama);
+
+//??
+
+//function 2 jenis
+// ada nilai balik
+// tanpa nilai balik
+
+//fungsi tanpa nilai balik
+// void hitung(int? a1, int? a2, int? a3) {
+//   num hasil = (a1 ?? 0 ) + (a2 ?? 0 ) + (a3 ?? 0);
+//   print('ini hasil $hasil');
+
+// }
+// hitung(100, 200, 300);
+// hitung(100, 200, null);
+// hitung(100, null, null);
+// hitung(null, 200, null);
+
+//fungsi ada nilai balik
+
+// String cetaknilailengkap(String nd, String? nb){
+//   var namalengkap = nd + ' ' + (nb ??'');
+//   return namalengkap;
+// }
+
+// String nama= cetaknilailengkap('wendy', 'wen');
+// String nama1= cetaknilailengkap('wendy', null);
+// print(nama);
+// print(nama1);
+
+// num hitung(int a1, int a2, int a3) {
+//   num hasil = a1 + a2 +a3;
+//   return hasil;
+// }
+
+// num nilai = hitung(100,200,300);
+// print(nilai);
+
+}
